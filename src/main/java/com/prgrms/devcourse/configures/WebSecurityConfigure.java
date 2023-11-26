f19846b5e6028c8b457d19e74313c981733ec456
ParkJuHan94 <1004sunye@naver.com>
2023-11-26 19:35:51 +0900

    feat: security config 에 user 추가  & remember-me 추가 & logout 추가

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -1,5 +1,6 @@
 package com.prgrms.devcourse.configures;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,40 +10,48 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 
 @Configuration
 @EnableWebSecurity
-public class WebSecurityConfigure extends
-    WebSecurityConfigurerAdapter {    // Spring Security 설정을 커스터마이징하기 위해 WebSecurityConfigurerAdapter 클래스를 확장
-
+@RequiredArgsConstructor
+public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
+    private PrgrmsUserDetailsService prgrmsUserDetailsService;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        super.configure(auth);
+
+        auth.inMemoryAuthentication()
+            .withUser("user").password("{noop}user123").roles("USER")
+            .and()
+            .withUser("admin").password("{noop}adnmin123").roles("ADMIN");
     }
 
     @Override
     public void configure(WebSecurity web) {
-        // 특정 경로 패턴(/assets/**)에 대한 Spring Security 보안 필터 체인을 적용하지 않도록 구성합니다.
-        // 주로 정적 리소스(이미지, CSS, JavaScript 등)에 사용됩니다.
+        // 특정 경로 패턴에 대한 Spring Security 보안 필터 체인을 적용하지 않도록 구성합니다.
         web.ignoring().antMatchers("/assets/**");
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
-            .authorizeRequests()
+            .authorizeRequests()    // authorizeRequests():HTTP 요청에 대한 보안을 구성하기 시작합니다.
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                .anyRequest().permitAll()
+                .anyRequest().permitAll() // .anyRequest().permitAll():위에서 정의되지 않은 모든 요청은 누구나 접근할 수 있도록 허용
                 .and()
-            .formLogin()
-                .defaultSuccessUrl("/")
+            .formLogin() // formLogin():폼 기반 로그인을 활성화
+                .defaultSuccessUrl("/") // .defaultSuccessUrl("/"):로그인 성공 시 리디렉션되는 기본 URL을 "/" 로 설정
+                .permitAll() // .permitAll():모든 사용자가 로그인 페이지에 접근할 수 있도록 설정
+                .and()
+            .rememberMe()
+                .rememberMeParameter("remember") // default: remember-me, checkbox 등의 이름과 맞춰야 함
+                .tokenValiditySeconds(300) // 쿠키의 만료시간 설정(초), default: 14일
+                .alwaysRemember(false) // 사용자가 체크박스를 활성화하지 않아도 항상 실행, default: false
+                .userDetailsService(prgrmsUserDetailsService) // 기능을 사용할 때 사용자 정보가 필요함. 반드시 이 설정 필요함.
+                .and()
+            .logout()
+                .logoutUrl("/logout") // Set custom logout URL
+                .logoutSuccessUrl("/") // Redirect to "/" after logout
                 .permitAll()
                 .and()
         ;
-//          authorizeRequests():HTTP 요청에 대한 보안을 구성하기 시작합니다.
-//          .antMatchers("/me").hasAnyRole("USER", "ADMIN"):URL 경로가 "/me" 인 경우, "USER" 또는 "ADMIN" 역할을 가진 사용자만 접근할 수 있도록 설정합니다.
-//          .anyRequest().permitAll():위에서 정의되지 않은 모든 요청은 누구나 접근할 수 있도록 허용합니다.
-//          formLogin():폼 기반 로그인을 활성화합니다.
-//          .defaultSuccessUrl("/"):로그인 성공 시 리디렉션되는 기본 URL을 "/" 로 설정합니다.
-//          .permitAll():모든 사용자가 로그인 페이지에 접근할 수 있도록 설정합니다.
     }
 
 }
