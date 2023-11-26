50fae0b3754db8f4aedd80979b5e755033398fb2
ParkJuHan94 <1004sunye@naver.com>
2023-11-26 19:34:39 +0900

    feat: (for remember-me)

---
+++ src/main/java/com/prgrms/devcourse/configures/PrgrmsUserDetailsService.java
@@ -0,0 +1,19 @@
+package com.prgrms.devcourse.configures;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+public class PrgrmsUserDetailsService implements UserDetailsService {
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        log.info("PrgrmsUserDetailsService loadUserByUsername " + username);
+        return null;
+    }
+
+}
