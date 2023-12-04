014083cbf6eaeb6020dafe6eff7ce40ca7e8a6c7
ParkJuHan94 <1004sunye@naver.com>
2023-12-04 14:22:01 +0900

    feat: 로그인 커스텀 (아이디 뒤 숫자 홀수만 Admin 권한 부여)

--- src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
+++ src/main/java/com/prgrms/devcourse/configures/WebSecurityConfigure.java
@@ -5,13 +5,17 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.SecurityExpressionHandler;
+import org.springframework.security.authentication.AuthenticationTrustResolverImpl;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.web.FilterInvocation;
 import org.springframework.security.web.access.AccessDeniedHandler;
 
 @Configuration
@@ -27,7 +31,9 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         auth.inMemoryAuthentication()
             .withUser("user").password("{noop}user123").roles("USER")
             .and()
-            .withUser("admin").password("{noop}admin123").roles("ADMIN");
+            .withUser("admin01").password("{noop}admin123").roles("ADMIN")
+            .and()
+            .withUser("admin02").password("{noop}admin123").roles("ADMIN");
     }
 
     @Override
@@ -41,8 +47,9 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         http
             .authorizeRequests()    // HTTP 요청에 대한 보안을 구성
                 .antMatchers("/me").hasAnyRole("USER", "ADMIN")
-                .antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated()")
+                .antMatchers("/admin").access("hasRole('ADMIN') and isFullyAuthenticated() and isOddAmin()")
                 .anyRequest().permitAll() // 위에서 정의되지 않은 모든 요청은 누구나 접근할 수 있도록 허용
+                .expressionHandler(expressionHandler())
                 .and()
             .formLogin() // 폼 기반 로그인을 활성화
                 .defaultSuccessUrl("/") // 로그인 성공 시 리디렉션되는 기본 URL을 "/" 로 설정
@@ -65,6 +72,20 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
             .requiresChannel()
                 .anyRequest().requiresSecure()
                 .and()
+            /*
+             * 세션 관리 설정을 시작합니다.
+             */
+            .sessionManagement()
+                .sessionFixation().changeSessionId() //  세션 고정 공격을 방지하기 위해 새 세션을 생성할 때마다 세션 ID를 변경
+                .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // 필요한 경우에만 세션을 생성
+                .invalidSessionUrl("/") // 유효하지 않은 세션(예: 만료된 세션)이 감지될 경우 사용자를 지정된 URL("/")로 리디렉션
+                .maximumSessions(1) // 한 사용자가 동시에 가질 수 있는 최대 세션 수를 1로 제한 -> 동일한 사용자 계정으로 여러 위치에서 동시 로그인을 방지
+                    .maxSessionsPreventsLogin(false) // 이미 최대 세션 수에 도달했을 때 추가 로그인을 차단하지 않도록 설정합니다. 대신 새 로그인이 발생하면 가장 오래된 세션을 종료합니다.
+                    .and()
+                .and()
+            /*
+             * 예외처리 핸들러
+             */
             .exceptionHandling()
                 .accessDeniedHandler(accessDeniedHandler())
 
@@ -85,4 +106,7 @@ public class WebSecurityConfigure extends WebSecurityConfigurerAdapter {
         };
     }
 
+    public SecurityExpressionHandler<FilterInvocation> expressionHandler() {
+        return new CustomWebSecurityExpressionHandler(new AuthenticationTrustResolverImpl(), "ROLE_");
+    }
 }
